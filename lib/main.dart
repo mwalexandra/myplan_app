25cf661cec0a45d11dc601c35480fc3dba65b252
mwalexandra <manik167@gmail.com>
2026-03-19 11:33:37 +0100

    Init commit: Firebase, Flutter dependencies, Fonts resolved

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyPlan - Kinder‑Tageskalender',
+      theme: ThemeData(
+        fontFamily: 'Quicksand',
+        useMaterial3: true,
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.w700,
+          ),
+          headlineMedium: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.w600,
+          ),
+          titleLarge: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w500,
+          ),
+          bodyLarge: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+          ),
+          bodySmall: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+            ),
+        ),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(title: const Text('QuickSand')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('displayLarge', style: Theme.of(context).textTheme.displayLarge),     // 32px w700
+            Text('headlineMedium', style: Theme.of(context).textTheme.headlineMedium), // 28px w600
+            Text('titleLarge', style: Theme.of(context).textTheme.titleLarge),         // 22px w500
+            Text('bodyLarge', style: Theme.of(context).textTheme.bodyLarge),           // 16px w400
+            Text('bodySmall', style: Theme.of(context).textTheme.bodySmall),         // 14px w400
+          ],
+        ),
+      ),
+    );
+  }
+}
